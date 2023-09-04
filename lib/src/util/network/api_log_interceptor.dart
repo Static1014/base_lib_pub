92018ee2bd4f30745abd7d47ccac3c482698ed18
Static1014 <420048248@qq.com>
2023-09-04 21:58:55 +0800

    1. 网络请求日志格式调整；

--- lib/src/util/network/api_log_interceptor.dart
+++ lib/src/util/network/api_log_interceptor.dart
@@ -7,11 +7,15 @@ import 'package:dio/dio.dart';
 /// Date : 2023/9/4 17:18
 class ApiLogInterceptor extends Interceptor {
   static const tag = 'Api Log';
+  static const String requestLine = '->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->';
+  static const String responseLine = '<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=';
+  static const String errorLine = '<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*<*';
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     // 如果你想完成请求并返回一些自定义数据，你可以使用 `handler.resolve(response)`。
     // 如果你想终止请求并触发一个错误，你可以使用 `handler.reject(error)`。
+    requestLine.logI(tag: tag, split: true);
     String url = globalDio.options.baseUrl + options.path;
     dynamic data = options.data;
     'Request  on url: $url, with ${data == null ? "no params" : "params:\n$data"}'.logI(tag: tag, split: true);
@@ -26,6 +30,7 @@ class ApiLogInterceptor extends Interceptor {
     dynamic data = err.requestOptions.data;
     'Error    on url: $url, with ${data == null ? "no params" : "params:\n$data"}  :\n$data'.logW(tag: tag, split: true);
     handler.next(err);
+    errorLine.logE(tag: tag, split: true);
   }
 
   @override
@@ -36,5 +41,6 @@ class ApiLogInterceptor extends Interceptor {
     dynamic data = response.data;
     'Response on url: $url, with response:\n$data'.logI(tag: tag, split: true);
     handler.next(response);
+    responseLine.logE(tag: tag, split: true);
   }
 }
